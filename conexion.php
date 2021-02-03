bb986542be7abea373e80431640c62f29caf7ad6
djkivv <djkivv@hotmail.com>
2021-02-03 21:19:49 +0100

    Modificacion BDConexion

--- conexion.php
+++ conexion.php
@@ -18,7 +18,7 @@
 $conexion = mysql_connect($IPservidor, $usuario, $clave) or die("<h2>No hay conexi&oacute;n con el servidor MySQL</h2>");
 mysql_query ("SET NAMES 'utf8'");
 mysql_select_db($nombreBD) or die('no se encontro la bd');
-*/
+
 if (!$mysqli) {
     $IPservidor = "sistema2020.codea.es";
     $nombreBD = "codea_sistema";
@@ -27,7 +27,7 @@ if (!$mysqli) {
     $mysqli = new mysqli($IPservidor, $usuario, $clave, $nombreBD);
     $mysqli->set_charset("utf8");
 }
-
+*/
 
 /*--------------------------------ESTA ES LA B.D DE WEB EN CODA-----------------*/
 if (!$mysqli2) {
